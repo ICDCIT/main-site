0774940485c6f33d1d40741dd5d40fae17ddf1ec
Srijan Saurav <sauravsrijan@gmail.com>
2019-01-02 17:54:26 +0530

    FIX: Add categories. Layout is fixed.

--- analyzer/core/urls.py
+++ analyzer/core/urls.py
@@ -5,5 +5,16 @@ app_name = 'core'
 
 urlpatterns = [
     path('',views.IndexView, name='index'),
-    path('bbsr_market/', views.BbsrMarket, name='bbsrmarket')
+    path('bbsr/', views.BbsrView, name='bbsr'),
+    path('agra/', views.AgraView, name='agra'),
+    path('bbsr/market/', views.BbsrMarket, name='bbsrmarket'),
+    path('bbsr/food/', views.BbsrFood, name='bbsrfood'),
+    path('bbsr/monument/', views.BbsrMonument, name='bbsrmonument'),
+    path('bbsr/Weather/', views.BbsrWeather, name='bbsrweather'),
+    path('bbsr/Crime/', views.BbsrCrime, name='bbsrcrime'),
+    path('agra/market/', views.AgraMarket, name='agramarket'),
+    path('agra/food/', views.AgraFood, name='agrafood'),
+    path('agra/monument/', views.AgraMonument, name='agramonument'),
+    path('agra/Weather/', views.AgraWeather, name='agraweather'),
+    path('agra/Crime/', views.AgraCrime, name='agracrime')
 ]
--- analyzer/core/views.py
+++ analyzer/core/views.py
@@ -3,5 +3,41 @@ from django.shortcuts import render
 
 def IndexView(request):
     return render(request, 'core/index.html')
+
+def BbsrView(request):
+    return render(request, 'core/bbsr.html')
+
+def AgraView(request):
+    return render(request, 'core/agra.html')
+
 def BbsrMarket(request):
     return render(request, 'bbsr/market.html')
+
+def BbsrFood(request):
+    return render(request, 'bbsr/food.html')
+
+def BbsrMonument(request):
+    return render(request, 'bbsr/monument.html')
+
+def BbsrWeather(request):
+    return render(request, 'bbsr/weather.html')
+
+def BbsrCrime(request):
+    return render(request, 'agra/crime.html')
+
+
+
+def AgraMarket(request):
+    return render(request, 'agra/market.html')
+
+def AgraFood(request):
+    return render(request, 'agra/food.html')
+
+def AgraMonument(request):
+    return render(request, 'agra/monument.html')
+
+def AgraWeather(request):
+    return render(request, 'agra/weather.html')
+
+def AgraCrime(request):
+    return render(request, 'agra/crime.html')
